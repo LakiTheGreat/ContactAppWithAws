bbb08c2e19571a7aefd81ba6918b0bafbd600a52
Nikola Lakobrija <nikola.lakobrija@enlightit.rs>
2023-11-06 16:21:15 +0100

    returned imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
-// import { Amplify } from "aws-amplify";
+import { Amplify } from "aws-amplify";
 import { Authenticator } from "@aws-amplify/ui-react";
 
 import App from "./App";
 import { CollapseDrawerProvider } from "./contexts/CollapseDrawerContext";
-// import awsExports from "./aws-exports";
+import awsExports from "./aws-exports";
 
-// Amplify.configure(awsExports);
+Amplify.configure(awsExports);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
