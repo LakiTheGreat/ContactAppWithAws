4f77024494eb4c17be403aa20966225878a40418
Nikola Lakobrija <nikola.lakobrija@enlightit.rs>
2023-11-12 09:14:47 +0100

    empty function

--- src/layouts/mainLayout/navbar/useGetNotificationContent.tsx
+++ src/layouts/mainLayout/navbar/useGetNotificationContent.tsx
@@ -1,14 +1,12 @@
-import mockedContacts from "__mocks__/mockedContacts.json";
-import getNotification from "utils/getNotificaton";
+// import mockedContacts from "__mocks__/mockedContacts.json";
+// import getNotification from "utils/getNotificaton";
 
 export default function useGetNotificationContent() {
-  const numberOfAllContacts: number = mockedContacts.length;
-  const numberOfFavoriteContacts: number = mockedContacts.filter(
-    (contact) => contact.isFavorite === true
-  ).length;
-
-  const allContacts = getNotification(numberOfAllContacts);
-  const suggestionNotification = getNotification(numberOfFavoriteContacts);
-
-  return [allContacts, suggestionNotification];
+  //   const numberOfAllContacts: number = mockedContacts.length;
+  //   const numberOfFavoriteContacts: number = mockedContacts.filter(
+  //     (contact) => contact.isFavorite === true
+  //   ).length;
+  //   const allContacts = getNotification(numberOfAllContacts);
+  //   const suggestionNotification = getNotification(numberOfFavoriteContacts);
+  //   return [allContacts, suggestionNotification];
 }
